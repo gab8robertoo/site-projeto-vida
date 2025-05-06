a0ba6b162dd5292b1035bc4c8e846c1033794896
gab8robertoo <145361926+gab8robertoo@users.noreply.github.com>
2025-05-06 11:30:33 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i < botoes.length; i++){
     botoes (i).onclik = function () {
-        for{ letj = 0; <botoes.length; j++){
+        for(letj = 0; <botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
        botoes[i].classList.add("ativo");
