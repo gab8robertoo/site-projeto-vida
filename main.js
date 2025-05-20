84a8941f3b6dcffde4371c455085dd5c262d73c7
gab8robertoo <145361926+gab8robertoo@users.noreply.github.com>
2025-05-20 11:37:38 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll ("aba-conteudo")
 
 for (let i = 0; i < botoes.length; i++){
     botoes (i).onclik = function () {
+       
         for(letj = 0; <botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
       
         botoes[i].classList.add("ativo");
-
-    }
+        textos[i].classList.add("ativo");
+   };
 }
+
+const contadores = document.querySelector
+const tempoObjetivo1 = new Date ("2025-08-23t00:00:00");
